0e943d8f12bbdc50dbbbcc961ec24c1e29c5cf04
Tianyu Du <tianyudu@stanford.edu>
2021-08-01 02:26:34 -0700

    update training procedure

--- src/train/train.py
+++ src/train/train.py
@@ -1,17 +1,22 @@
 """
-The basic training loop.
+A prototypical training procedure for training deep-choice models.
 """
 import argparse
 import copy
-import os
 from typing import List
 
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch import nn, optim
+import torch.nn.functional as F
+from torch import optim
 
 import train.utils as utils
+
+
+def accuracy(outputs: torch.Tensor, labels: torch.Tensor) -> float:
+    _, preds = torch.max(outputs, dim=1)
+    acc = torch.tensor(torch.sum(preds == labels).item() / len(preds)) * 100
+    return float(acc)
 
 
 def train(datasets: List[dict],
@@ -33,13 +38,9 @@ def train(datasets: List[dict],
     data_train, data_val, data_test = datasets
     do_validation = data_val is not None
     do_test = data_test is not None
-    
-    X, user_onehot, A, Y, C = (data_train['X'], data_train['user_onehot'],
-                               data_train['A'], data_train['Y'], data_train['C'])
 
-    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=args.lr_decay)
-    loss_fn = nn.CrossEntropyLoss()
 
     last_model = None
     param_norm_change = 1E10
@@ -48,66 +49,81 @@ def train(datasets: List[dict],
     # record training progress.
     loss_list, acc_list, ll_list = [], [], []
 
-    if args.batch_size == -1:
-        args.batch_size = len(Y)
-
     # run until convergence in terms of parameter.
     while (param_norm_change > args.param_norm_eps) and (epoch <= args.max_epochs):
         epoch += 1
         # record metrics across batches.
         epoch_loss, epoch_acc = list(), list()
         log_likelihood = 0.0
+        model.train()
+        for features, y_batch in data_train:
+            X_batch, user_onehot_batch, A_batch, C_batch = features
 
-        for i in range(len(Y) // args.batch_size):
-            first, last = i * args.batch_size, min(len(Y), (i + 1) * args.batch_size)
-            index = torch.arange(first, last)
-
-            X_batch = dict()
-            for k, x in X.items():
-                if isinstance(x, torch.Tensor):
-                    X_batch[k] = x[index, :, :]
-                else:
-                    X_batch[k] = None
+            y_batch = y_batch.to(args.device)
+            for k, v in X_batch.items():
+                if v is not None:
+                    X_batch[k] = v.to(args.device)
             
-            y_pred = model(X_batch, A[index, :], user_onehot[index])
-            loss = loss_fn(y_pred, Y[index])
+            y_pred = model(X_batch, availability=A_batch, user_onehot=user_onehot_batch)
+            loss = F.cross_entropy(y_pred, y_batch, reduction='mean')
+            log_likelihood -= float(loss.item()) * len(y_batch)
 
-            # get log-likelihood.
-            f = nn.Softmax(dim=1)
-            prob = f(y_pred).detach()
-            log_likelihood += torch.log(prob[torch.arange(len(Y)), Y] + 1e-6).sum()
-            # another way of computing it.
-            # log_likelihood -= float(loss.item()) * len(index)
-
-            epoch_loss.append(loss.item())
-            epoch_acc.append((y_pred.argmax(dim=1) == Y[index]).float().mean().item() * 100)
-
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            scheduler.step()
-       
-        # track the status of convergence.
+
+            epoch_loss.append(loss.item())
+            epoch_acc.append(accuracy(outputs=y_pred, labels=y_batch))
+        # change learning rate after processing all batches, which should leads to faster convergence.
+        # see https://discuss.pytorch.org/t/scheduler-step-after-each-epoch-or-after-each-minibatch/111249
+        # for scheduling after each epoch or each mini-batch.
+        scheduler.step()
+        # assess the status of convergence.
         if last_model is None:
             param_norm_change = 1E10
         else:
             param_norm_change = utils.diff_in_norm(last_model, model.state_dict())
         last_model = copy.deepcopy(model.state_dict())
- 
+
+        # average across mini-batches get training loss for the current epoch.
         loss_list.append(np.mean(epoch_loss))
         acc_list.append(np.mean(epoch_acc))
-        ll_list.append(log_likelihood.detach().cpu())
+        ll_list.append(log_likelihood)
+
         if epoch % args.eval_epoch == 0:
-            print(f'[Epoch={epoch} Train] LL={log_likelihood:5f}, ACC={np.mean(epoch_acc):5f}%, LOSS={np.mean(epoch_loss):5f}, NORM DELTA={param_norm_change:5f}')
+            out = f'[Epoch={epoch} Train] LL={log_likelihood:6f}, ACC={np.mean(epoch_acc):5f}%, LOSS={np.mean(epoch_loss):5f}, NORM DELTA={param_norm_change:5f}'
+            if do_validation:
+                perf = eval_step(model, data_val, args)
+                out += f"[Val] LL={perf['log_likelihood']:5f}, ACC={perf['accuracy']:5f}%, LOSS={perf['loss']:5f}"
+            if do_test:
+                perf = eval_step(model, data_test, args)
+                out += f"[Test] LL={perf['log_likelihood']:5f}, ACC={perf['accuracy']:5f}%, LOSS={perf['loss']:5f}" 
+            print(out)
 
-    torch.save(model, os.path.join(args.out_dir, 'model.pt'))
-    torch.save(prob, os.path.join(args.out_dir, 'prob.pt'))
-    print(prob.mean(dim=0))
 
-    # summary plot.
-    fig, axes = plt.subplots(ncols=3, figsize=(24, 8))
-    axes[0].plot(loss_list, label='loss')
-    axes[1].plot(acc_list, label='accuracy')
-    axes[2].plot(ll_list, label='log-likelihood')
-    for ax in axes:
-        ax.legend()
-    fig.savefig(os.path.join(args.out_dir, 'curve.png'))
+@torch.no_grad()
+def eval_step(model, dataset, args) -> dict:
+    """Evaluate the model on either the validation or testing set, returns a dictionary of
+    evaluation metrics.
+    """
+    model.eval()
+    log_likelihood = 0.0
+    # average across batches, but in most cases, validation and test datasets should only have 1 batch.
+    loss_list, acc_list = list(), list()
+    for features, y_batch in dataset:
+        X_batch, user_onehot_batch, A_batch, C_batch = features
+
+        y_batch = y_batch.to(args.device)
+        for k, v in X_batch.items():
+            if v is not None:
+                X_batch[k] = v.to(args.device)        
+
+        y_pred = model(X_batch, availability=A_batch, user_onehot=user_onehot_batch)
+
+        loss = F.cross_entropy(y_pred, y_batch, reduction='mean')
+        loss_list.append(float(loss.item()))
+        log_likelihood -= float(loss.item()) * len(y_batch)
+        acc_list.append(accuracy(outputs=y_pred, labels=y_batch))
+    loss = np.mean(loss_list)
+    acc = np.mean(acc_list)
+    return {'log_likelihood': log_likelihood, 'loss': loss, 'accuracy': acc}
