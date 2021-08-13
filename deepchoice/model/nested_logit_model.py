9d3c4c940a1462f4bf8d1807ea84aaf1c80fd21c
Tianyu Du <tianyudu@stanford.edu>
2021-08-13 11:15:19 -0700

    fix bug

--- deepchoice/model/nested_logit_model.py
+++ deepchoice/model/nested_logit_model.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import warnings
 
-from coefficient import Coefficient
+from deepchoice.model.coefficient import Coefficient
 
 
 class NestedLogitModel(nn.Module):
