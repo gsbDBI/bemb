30ece4f40dc6438fc9ecde405466151cbe44a8e8
Tianyu Du <tianyudu@stanford.edu>
2021-09-28 00:06:39 -0700

    update __init__

--- deepchoice/model/__init__.py
+++ deepchoice/model/__init__.py
@@ -2,4 +2,6 @@ from .conditional_logit_model import *
 from .nested_logit_model import *
 from .bemb import *
 from .bemb_flex import *
+from .bemb_lightning import *
+from .bemb_flex_lightning import *
 # from .nested_bemb import *
