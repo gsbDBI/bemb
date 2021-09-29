c2c83ee270b0f4e21f888978c088b4802ce1cae1
Tianyu Du <tianyudu@stanford.edu>
2021-09-29 16:59:15 -0700

    use bemb flex v2 by default

--- deepchoice/model/__init__.py
+++ deepchoice/model/__init__.py
@@ -1,7 +1,8 @@
 from .conditional_logit_model import *
 from .nested_logit_model import *
 from .bemb import *
-from .bemb_flex import *
+# from .bemb_flex import *
+from .bemb_flex_v2 import *
 from .bemb_lightning import *
 from .bemb_flex_lightning import *
 # from .nested_bemb import *
