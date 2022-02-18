ad8b6370eab59548a4c710f00cedbcb2c7a210b9
Tianyu Du <tianyudu@stanford.edu>
2022-02-18 10:33:29 -0800

    ad file

---
+++ tutorials/psid/split_psid_transitions.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+if __name__ == '__main__':
+    psid_all_transitions = pd.read_csv('/home/tianyudu/Development/PSID/psid_public_dataset.csv', low_memory=False)
+    output_path = './psid_by_decade/'
+    # Year_1 values are 2-digit.
+    psid_all_transitions['Decade_1'] = (psid_all_transitions['Year_1'] // 10 * 10)
+    for decade in psid_all_transitions['Decade_1'].unique():
+        psid_decade_transitions = psid_all_transitions[psid_all_transitions['Decade_1'] == decade]
+        print(f'psid_{decade}s', len(psid_decade_transitions), 'transitions')
+        psid_decade_transitions.to_csv(os.path.join(output_path, f'psid_{decade}s'), index=False)
